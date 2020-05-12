b4880d0db485ee89ac1d67b8aeaccbd6b91cea77
Lais Omena <laisfrancelly7@gmail.com>
2020-05-12 17:15:23 +0100

    Auto stash before merge of "master" and "PHP/master"

--- array.php
+++ array.php
@@ -2,6 +2,17 @@
 
 $idadeList = [19, 18, 23, 20, 25, 30, 21, 22, 19];
 
+//$idadeLais = $idadeList[3];
+//$idadeArthur = $idadeList[6];
+//$idadeZoe = $idadeList[5];
+
+list($idadeLais, , $idadeArthur, $idadeZoe) = $idadeList;
+
+//adiciona na próxima posição vazia
+$idadeList[] = 20;
+
 for ($i=0; $i < count($idadeList); $i++){
     echo $idadeList[$i] . PHP_EOL;
 }
+
+echo "$idadeLais $idadeArthur $idadeZoe";
--- banco.php
+++ banco.php
@@ -1,27 +1,44 @@
 <?php
 
+require_once 'funcoes.php';
+
 $contasCorrentes = [
-    12345678910 => ['titular' => 'Laís', 'saldo' => 5000],
-    98765432100 => ['titular' => 'Arthur', 'saldo' => 5000],
-    63473479812 => ['titular' => 'Flora', 'saldo' => 1500]
+    '123.456.789-10' => ['titular' => 'Laís', 'saldo' => 5000],
+    '987.654.321-00' => ['titular' => 'Arthur', 'saldo' => 5000],
+    '634.734.798-12' => ['titular' => 'Flora', 'saldo' => 1500]
 ];
 
-$contasCorrentes = sacar($contasCorrentes[12345678910], 500);
+$contasCorrentes['634.734.798-12'] = sacar(500, $contasCorrentes['634.734.798-12']);
+$contasCorrentes['123.456.789-10'] = sacar(400, $contasCorrentes['123.456.789-10']);
+$contasCorrentes['987.654.321-00'] = depositar($contasCorrentes['987.654.321-00'], 900);
+nameBold($contasCorrentes['987.654.321-00']);
 
-foreach ($contasCorrentes as $cpf => $conta){
-     exibeMensagem($cpf . ' ' . $conta['titular'] . ' ' . $conta['saldo']);
-}
+//remove variável da memória
+unset($contasCorrentes['634.734.798-12']);
 
-function exibeMensagem($mensagem){
-    echo $mensagem . PHP_EOL;
-}
+?>
 
-function sacar($valor, $conta){
-    if($valor > $conta['saldo']){
-        exibeMensagem('Saldo insuficiente');
-    }else{
-        $conta['saldo'] -= $valor;
-        exibeMensagem('Saque realizado com sucesso');
-    }
-    return $conta;
-}
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Contas correntes</h1>
+
+    <dl>
+        <?php foreach($contasCorrentes as $cpf => $conta) { ?>
+        <dt>
+            <h3><?=$conta['titular'] ?> - <?=$cpf; ?></h3>
+        </dt>
+        <dd>
+            Saldo: <?=$conta['saldo']; ?>
+        </dd>
+        <?php } ?>
+    </dl>
+</body>
+</html>
