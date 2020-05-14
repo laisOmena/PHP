487c5aaa2f267c62b822d5bab3fe2305156c5027
Lais Omena <laisfrancelly7@gmail.com>
2020-05-14 21:44:25 +0100

    POO
    criando get e set, testando

---
+++ POO/banco.php
@@ -0,0 +1,13 @@
+<?php
+
+require_once 'src/Conta.php';
+
+$conta1 = new Conta('123.456.789-10', 'Laís');
+$conta2 = new Conta('987.654.321-10', 'Arthur');
+
+$conta1->deposit(300);
+$conta1->transfer(50, $conta2);
+$conta2->withdraw(25);
+
+echo $conta1->getBalance() . PHP_EOL;
+echo $conta2->getBalance();
---
+++ POO/src/Conta.php
@@ -0,0 +1,98 @@
+<?php
+
+
+class Conta
+{
+    private $cpfHolder;
+    private $nameHolder;
+    private $balance;
+
+    /**
+     * Conta constructor.
+     * @param string $cpfHolder
+     * @param string $nameHolder
+     */
+    public function __construct(string $cpfHolder, string $nameHolder)
+    {
+        $this->cpfHolder = $cpfHolder;
+        $this->nameHolder = $nameHolder;
+        $this->validateHolderName($nameHolder);
+        $this->balance = 0;
+    }
+    /**
+     * @return mixed
+     */
+    public function getCpfHolder()
+    {
+        return $this->cpfHolder;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getNameHolder()
+    {
+        return $this->nameHolder;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getBalance()
+    {
+        return $this->balance;
+    }
+
+    /**
+     * @param $this
+     * @param float $value
+     */
+    public function withdraw(float $value): void
+    {
+        if ($value > $this->balance){
+            echo "saldo indisponível";
+            return;
+        }
+        $this->balance -= $value;
+
+    }
+
+    /**
+     * @param float $value
+     */
+    public function deposit(float $value): void
+    {
+        if ($value < 0){
+            echo "Operação inválida";
+            return;
+        }
+        $this->balance += $value;
+    }
+
+    /**
+     * @param float $value
+     * @param Conta $targetAccount
+     */
+    public function transfer(float $value, Conta $targetAccount): void
+    {
+        if ($value > $this->balance){
+            echo "Saldo insuficiente";
+            return;
+        }
+        $this->withdraw($value);
+        $targetAccount->deposit($value);
+
+    }
+
+    /**
+     * @param string $nomeTitular
+     */
+    private function validateHolderName(string $nomeTitular)
+    {
+        if(strlen($nomeTitular) < 5){
+            echo "Nome inválido, insira sobrenome";
+            exit();
+        }
+    }
+
+}
